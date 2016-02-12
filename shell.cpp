1ef3ebffbea90b80518279c5d26959a6057aecf0
jcs4te <jcs4te@github.com>
2016-02-12 14:49:40 -0500

    running one commmand

--- shell.cpp
+++ shell.cpp
@@ -3,10 +3,29 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <ctype.h>
 #include <cstring>
 
 int count = 0;
+
+void executer(char **args) {
+	pid_t pid;
+	int cur_status;
+	pid = fork();
+	if (pid < 0) {
+		printf("ERROR: Could not fork children\n");
+		exit(1);
+	} else if (pid == 0) {
+		//printf("\n");
+		execvp(*args, args);
+			printf("ERROR: Exec failed\n");
+			exit(1);
+		}
+		else {
+			while(wait(&cur_status) != pid);
+		}
+}
 
 void parse(char *input, char **args) {
 	while (*input != '\0') {
@@ -29,13 +48,16 @@ int main() {
 	while(1) {
 		printf("$> ");
 		gets(input);
-		printf("\n");
-		count = 0;
 		parse(input, args);
-		for (int i = 0; i < count; i++) {
-			printf("%s\n", args[i]);
+		if (strcmp(args[0], "exit") == 0) {
+			exit(0);
 		}
+		/*for (int i = 0; i < count; i++) {
+			printf("%s\n", args[i]);
+		}*/
+		executer(args);
 
 	}
+
 	return 0;
 }
